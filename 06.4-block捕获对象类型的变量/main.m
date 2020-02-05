b78affd9d69eed78898ad5fe3d9f258a184a3a46
刘光强 <1126756952@qq.com>
2020-02-05 17:12:18 +0800

    Initial Commit

---
+++ 06.4-block捕获对象类型的变量/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  06.4-block捕获对象类型的变量
+//
+//  Created by 刘光强 on 2020/2/5.
+//  Copyright © 2020 guangqiang.liu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
